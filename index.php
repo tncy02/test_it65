65ca1941c2d9f05b886228592220c7d92f63982c
tncy02 <65309010002@chontech.ac.th>
2023-01-07 09:53:14 +0700

    new many file

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    include "header.php";
+    include "banner.php";
+    include "navbar.php";
+    include "conn.php";
+?>
+
+<div class="container mt-5">
+ 
+<center>
+  <h1>Chayapol Saleephol</h1>
+</center>
+
+</div>
+
+
+</body>
+
+<?php
+    include "footer.php";
+?>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,20 @@
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="index.php">Home</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link active" href="showStd.php">Show Student <Datag></Datag></a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+<!-- <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Fixed top</a>
+  </div>
+</nav> -->
